1dfaf88d64100db3f72b3e35589fc99af9a852b4
Daniel juliao <zigmainflables@gmail.com>
2021-08-21 00:51:23 -0500

    creando Entidades

---
+++ src/main/java/web/controller/AdiestramientoApplication.java
@@ -0,0 +1,13 @@
+package web.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdiestramientoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdiestramientoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Adiestramiento/Workingdog/AdiestramientoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Adiestramiento.Workingdog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdiestramientoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
